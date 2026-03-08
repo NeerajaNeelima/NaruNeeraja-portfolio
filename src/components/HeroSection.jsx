85c33c7b3dad70f23e13b7727235c6fa298dea11
Naru Neeraja <023neeraja@gmail.com>
2026-03-08 09:37:30 +0530

    fix: fixed eslint warnings

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -71,7 +71,7 @@ performance. Open to relocation.
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[390px] lg:h-[390px] relative">
             <img
               src="/images/hero-image.png"
-              alt="hero image"
+              alt="hero"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               width={300}
               height={300}
